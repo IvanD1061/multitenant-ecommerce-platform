eac5f9e454ec751ee51c0d07d0f2f7f533c24658
IvanD1061 <ivan.duenas@wsu.edu>
2026-03-06 16:04:55 -0800

    added product list UI

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,52 +1,33 @@
 import { loadProductFilter } from "@/modules/products/search-params";
-import { ProductFilters } from "@/modules/products/ui/components/product-filters";
-import { ProductList,ProductListSkeleton } from "@/modules/products/ui/components/product-list";
-import { ProductSort } from "@/modules/products/ui/components/product-sort";
+import { ProductListView } from "@/modules/products/ui/views/product-list-view";
 import { caller, getQueryClient, trpc } from "@/trpc/server";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { SearchParams } from "nuqs/server";
-import { Suspense } from "react";
 
-interface Props{
+
+interface Props {
     params: Promise<{
-        category : string;
+        category: string;
     }>,
     searchParams: Promise<SearchParams>;
-    
+
 }
 
-const Page = async ({params,searchParams }: Props) => {
-    const { category} = await params;
+const Page = async ({ params, searchParams }: Props) => {
+    const { category } = await params;
     const filters = await loadProductFilter(searchParams)
-
-    console.log(JSON.stringify(filters), "THIS IS FROM RSC")
 
     const queryClient = getQueryClient()
     void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
         category,
+        ...filters,
     }))
 
 
-    return(   
-           <HydrationBoundary state ={dehydrate(queryClient)}>
-            <div className="px-4 lg:px-12 py-8 flex flex-col gap-4" >
-                <div className="flex flex-col lg:flex-row ig;items-center gap-y-2 lg:gap-y-0 justify-between">
-                    <p className="text-2xl font-medium">Curated for you</p>
-                    <ProductSort></ProductSort>
-
-                </div>
-                <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12">
-                <div className="lg:col-span-2 xl:col-span-2">
-               <ProductFilters/>
-                </div>
-                <div className="lg:col-span-4 xl:col-span-6">
-                <Suspense fallback ={<ProductListSkeleton/>}>
-                    <ProductList category={category}/>
-                </Suspense>
-                </div>
-                </div>
-        </div>
-            </HydrationBoundary>
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <ProductListView category={category} />
+        </HydrationBoundary>
     )
 }
 
