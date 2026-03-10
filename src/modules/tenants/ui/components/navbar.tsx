acb6f147c78dd578f9559641c5b4cc0674fca64b
IvanD1061 <ivan.duenas@wsu.edu>
2026-03-10 16:13:43 -0700

    added the Cart checkout Features

--- src/modules/tenants/ui/components/navbar.tsx
+++ src/modules/tenants/ui/components/navbar.tsx
@@ -4,6 +4,23 @@ import { useSuspenseQuery } from "@tanstack/react-query"
 import Link from "next/link";
 import Image from "next/image";
 import { generateTenantURL } from "@/lib/utils";
+import dynamic from "next/dynamic";
+import { Button } from "@/components/ui/button";
+import { ShoppingCartIcon } from "lucide-react";
+
+const CheckoutButton = dynamic(
+    () => import("@/modules/checkout/ui/components/checkout-button").then(
+        (mod) => mod.CheckoutButton,
+    ),
+    {
+        ssr: false,
+        loading: () => <Button disabled className="bg-white">
+            <ShoppingCartIcon className="text-black" />
+
+        </Button>
+    }
+)
+
 interface Props {
     slug: string;
 }
@@ -29,6 +46,8 @@ export const Navbar = ({ slug }: Props) => {
                     )}
                     <p className="text-xl"> {data.name}</p>
                 </Link>
+                <CheckoutButton hideIfEmpty tenantSlug={slug} />
+
 
             </div>
 
@@ -39,12 +58,12 @@ export const Navbar = ({ slug }: Props) => {
 export const NavbarSkeleton = () => {
     return (
         <nav className=" h-20 border-b font-medium bg-white">
-            <div
-                className=" max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full px-4 lg:px-12">
-
-
+            <div className=" max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full px-4 lg:px-12">
+                <div />
+                <Button disabled className="bg-white">
+                    <ShoppingCartIcon className="text-black" />
+                </Button>
             </div>
-
         </nav>
     )
 }
